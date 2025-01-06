b21c587f8f4adb35b7316d5628ee9536b1b25825
Datsteelfn2 <Datsteelfn@gmail.com>
2025-01-05 21:15:21 -0500

    managed to get the string to print on same line, use var='' as second print arg

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 sentance=input("What do you sentance do you want to rainbonize:> ")
-colors=['r','g']
+colors=['r','g',' ']
 for letter in sentance:
     if letter.lower()==colors[0]:#red
-        print('\033[31m')
+        print('\033[31m',end='')
         
     elif letter.lower()==colors[1]:
-        print('\033[32m')#green
-    print(letter)
+        print('\033[32m',end='')#green
+    elif letter==colors[2]:
+        print('\033[0m',end ='')
+    print(letter,end='')
